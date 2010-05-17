25b0305e95b0a6dd40280745741192719daf2ef1
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-05-17 11:20:34 +0100

    Amend cpconf.pl to add mirror option

--- cpconf.pl
+++ cpconf.pl
@@ -5,9 +5,9 @@ use CPANPLUS::Configure;
 
 my $mx;
 my $email;
-my $protocol;
+my $mirror;
 
-GetOptions( 'mx=s', \$mx, 'email=s', \$email, 'protocol=s', \$protocol );
+GetOptions( 'mx=s', \$mx, 'email=s', \$email, 'mirror=s', \$mirror );
 
 die "No --email specified, please do so\n" unless $email;
 
@@ -21,39 +21,16 @@ $conf->set_conf( buildflags => 'uninst=1' );
 $conf->set_conf( enable_custom_sources => 0 );
 $conf->set_conf( show_startup_tip => 0 );
 $conf->set_conf( write_install_logs => 0 );
-my $hosts = $conf->get_conf( 'hosts' );
-unshift @$hosts, _hosts( $protocol );
-$conf->set_conf( hosts => $hosts );
+if ( $mirror ) {
+  my $hosts = $conf->get_conf( 'hosts' );
+  my @parts = $mirror =~ m|^(\w*)://([^/]*)(/.*)$|s;
+  my $href;
+  for my $key (qw[scheme host path]) {
+    $href->{$key} = shift @parts;
+  }
+  unshift @$hosts, $href;
+  $conf->set_conf( hosts => $hosts );
+}
 $conf->save();
 exit 0;
 
-sub _hosts {
-  my $prot = shift || 'http';
-  return 
-   (
-    {
-      scheme => 'ftp',
-      host   => 'cpan.hexten.net',
-      path   => '/',
-    },
-    {
-      scheme => 'ftp',
-      host   => 'cpan.cpantesters.org',
-      path   => '/CPAN/',
-    },
-   ) if $prot eq 'ftp';
-
-  return 
-   (
-    {
-      scheme => 'http',
-      host   => 'cpan.hexten.net',
-      path   => '/',
-    },
-    {
-      scheme => 'http',
-      host   => 'cpan.cpantesters.org',
-      path   => '/',
-    },
-   );
-}
