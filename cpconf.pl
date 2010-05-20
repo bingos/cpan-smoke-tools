7341186b77a14ab746a185018ece26c63f429b72
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-05-20 12:19:55 +0100

    Make cpconf.pl set some sane CPAN mirrors

--- cpconf.pl
+++ cpconf.pl
@@ -7,6 +7,12 @@ my $mx;
 my $email;
 my $mirror;
 
+my @mirrors = (
+  'http://cpan.hexten.net/',
+  'http://cpan.cpantesters.org/',
+  'ftp://ftp.funet.fi/pub/CPAN/',
+);
+
 GetOptions( 'mx=s', \$mx, 'email=s', \$email, 'mirror=s', \$mirror );
 
 die "No --email specified, please do so\n" unless $email;
@@ -21,16 +27,17 @@ $conf->set_conf( buildflags => 'uninst=1' );
 $conf->set_conf( enable_custom_sources => 0 );
 $conf->set_conf( show_startup_tip => 0 );
 $conf->set_conf( write_install_logs => 0 );
-if ( $mirror ) {
-  my $hosts = $conf->get_conf( 'hosts' );
-  my @parts = $mirror =~ m|^(\w*)://([^/]*)(/.*)$|s;
+unshift @mirrors, $mirror if $mirror;
+my $hosts = [ ];
+for ( @mirrors ) {
+  my @parts = $_ =~ m|^(\w*)://([^/]*)(/.*)$|s;
   my $href;
   for my $key (qw[scheme host path]) {
     $href->{$key} = shift @parts;
   }
   unshift @$hosts, $href;
-  $conf->set_conf( hosts => $hosts );
 }
+$conf->set_conf( hosts => $hosts );
 $conf->save();
 exit 0;
 
