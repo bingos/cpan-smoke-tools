6868789dbd611997c9e52890f0cb20f5dbda3efd
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-05-21 12:26:30 +0100

    Added transporter socket options to cpconf.pl

--- cpconf.pl
+++ cpconf.pl
@@ -2,10 +2,13 @@ use strict;
 use warnings;
 use Getopt::Long;
 use CPANPLUS::Configure;
+use Module::Load::Conditional qw[check_install];
 
 my $mx;
 my $email;
 my $mirror;
+my $socket;
+my $port;
 
 my @mirrors = (
   'http://cpan.hexten.net/',
@@ -13,7 +16,7 @@ my @mirrors = (
   'ftp://ftp.funet.fi/pub/CPAN/',
 );
 
-GetOptions( 'mx=s', \$mx, 'email=s', \$email, 'mirror=s', \$mirror );
+GetOptions( 'mx=s', \$mx, 'email=s', \$email, 'mirror=s', \$mirror, 'socket=s', \$socket, 'port=s' => \$port );
 
 die "No --email specified, please do so\n" unless $email;
 
@@ -37,6 +40,10 @@ for ( @mirrors ) {
   }
   push @$hosts, $href;
 }
+
+$conf->set_conf( cpantest_reporter_args => { transport => 'Socket', transport_args => [ host => $socket, port => $port ] } )
+  if $socket and $port and check_install( module => 'Test::Reporter::Transport::Socket' );
+
 $conf->set_conf( hosts => $hosts );
 $conf->save();
 exit 0;
