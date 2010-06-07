be3ae38a92d9058ad2e1dd4a8e7f4b2e09d74cc8
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-06-07 10:50:29 +0100

    Added metabase socket configuration script

---
+++ metabase.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+use Getopt::Long;
+use CPANPLUS::Configure;
+use Module::Load::Conditional qw[check_install];
+
+my $relay;
+my $port;
+
+GetOptions( 'relay=s', \$relay, 'port=s' => \$port );
+
+die "No --relay or --port specified, please do so\n" unless $relay and $port;
+
+my $conf = CPANPLUS::Configure->new();
+$conf->set_conf( cpantest_reporter_args => { transport => 'Socket', transport_args => [ host => $relay, port => $port ] } )
+  if $socket and $port and check_install( module => 'Test::Reporter::Transport::Socket' );
+$conf->save();
+exit 0;
