7e263c4514dd60fd8ce87a9db0b4aa76fb994186
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-08-27 20:10:04 +0100

    Added mirror conf tools

---
+++ mirrors.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use Getopt::Long;
+use CPANPLUS::Configure;
+
+my $mirror;
+my @mirrors = (
+  'http://cpan.hexten.net/',
+  'http://cpan.cpantesters.org/',
+  'ftp://ftp.funet.fi/pub/CPAN/',
+);
+
+GetOptions( 'mirror=s', \$mirror, );
+
+my $conf = CPANPLUS::Configure->new();
+unshift @mirrors, $mirror if $mirror;
+my $hosts = [ ];
+for ( @mirrors ) {
+  my @parts = $_ =~ m|^(\w*)://([^/]*)(/.*)$|s;
+  my $href;
+  for my $key (qw[scheme host path]) {
+    $href->{$key} = shift @parts;
+  }
+  push @$hosts, $href;
+}
+
+$conf->set_conf( hosts => $hosts );
+$conf->save();
+exit 0;
