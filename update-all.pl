9dd511814444437111d14eb3c0cbd261eb6cc815
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-10-30 10:15:03 +0100

    Add perl version sort to update-all script

--- update-all.pl
+++ update-all.pl
@@ -3,6 +3,7 @@ use warnings;
 use File::Spec;
 use Capture::Tiny qw[capture_merged];
 use Cwd;
+use Perl::Version;
 use FindBin qw[$Bin];
 
 die unless @ARGV;
@@ -23,7 +24,7 @@ foreach my $arg ( @ARGV ) {
   }
   closedir $dir;
   next unless @perls;
-  foreach my $perl ( sort @perls ) {
+  foreach my $perl ( sort _version_sort @perls ) {
     my $conf = File::Spec->catdir( $confroot, $perl );
     next unless -d $conf;
     my $perlexe = File::Spec->catfile($path,$perl,'bin','perl');
@@ -42,3 +43,7 @@ foreach my $arg ( @ARGV ) {
   }
 }
 exit 0;
+
+sub _version_sort {
+  Perl::Version->new( ( split /-/, $a )[1] )->numify <=> Perl::Version->new( ( split /-/, $b )[1] )->numify
+}
