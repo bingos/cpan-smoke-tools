b48c010e5aa18100c2f0b8332756a9b25afd8566
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-12-24 01:01:40 +0000

    Script to clear /tmp

---
+++ zaptmp.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use File::Path qw[rmtree];
+use File::Spec;
+
+my $tmpdir = File::Spec->tmpdir;
+my $name   = 'cpan';
+my $uid    = getpwnam($name);
+my $now    = time();
+
+{
+  opendir( my $TMPDIR, $tmpdir ) or die "$!\n";
+  while (my $item = readdir($TMPDIR)) {
+    next if $item =~ /^\.{1,2}$/;
+    my $file = File::Spec->catfile($tmpdir,$item);
+    my ($fuid,$mtime) = ( stat($file) )[4,9];
+    next unless $uid == $fuid; # and $now - $mtime > 86400;
+    print $file, "\n";
+    rmtree( $file );
+  }
+}
