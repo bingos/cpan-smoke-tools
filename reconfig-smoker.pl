2b3c9ceed4c02c7acedf3ab828903925d06c3ad8
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-09-15 21:27:51 +0100

    He was like 'Woah' and I was like 'Yeah!' and then the coconut fell

--- reconfig-smoker.pl
+++ reconfig-smoker.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use CPANPLUS::Configure;
 my $conf = CPANPLUS::Configure->new();
-$conf->set_conf( prefer_bin => 1 );
+#$conf->set_conf( prefer_bin => 1 );
+$conf->set_program( make => '/usr/bin/gmake' );
 $conf->save();
 exit 0;
