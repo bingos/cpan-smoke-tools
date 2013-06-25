577233b7a79b461852c15ae123ecdeaeba402769
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-06-25 12:33:49 +0100

    Don't use curl on Cygwin for the moment.

--- task-bingos-bootstrap.pl
+++ task-bingos-bootstrap.pl
@@ -1,3 +1,8 @@
 use strict;
 use warnings;
-exec "curl -kL http://cpanmin.us | $^X - -v --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
+if ( $^O eq 'cygwin' ) {
+  exec "wget --no-check-certificate -O - http://cpanmin.us | $^X - -v --no-curl --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
+}
+else {
+  exec "curl -kL http://cpanmin.us | $^X - -v --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
+}
