71f4206e6306c1e33aa017714a3883f5fabb4377
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2015-06-04 14:14:17 +0100

    Use perl instead of sed for substitution
    Perl is consistent across different platforms and sed is not
    
    See https://github.com/miyagawa/cpanminus/issues/462

--- task-bingos-bootstrap.pl
+++ task-bingos-bootstrap.pl
@@ -1,8 +1,8 @@
 use strict;
 use warnings;
 if ( $^O eq 'cygwin' ) {
-  exec "wget --no-check-certificate -O - http://cpanmin.us | sed -e 's/local\$self->{notest}=1;//g' | $^X - -v --no-curl --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
+  exec "wget --no-check-certificate -O - http://cpanmin.us | $^X -pe 's/local\\\$self->{notest}=1;//g' | $^X - -v --no-curl --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
 }
 else {
-  exec "curl -kL http://cpanmin.us | sed -e 's/local\$self->{notest}=1;//g' | $^X - -v --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
+  exec "curl -kL http://cpanmin.us | $^X -pe 's/local\\\$self->{notest}=1;//g' | $^X - -v --without-recommends --mirror http://cpan.mirror.local/CPAN/ --mirror-only Task::BINGOS::Bootstrap";
 }
